4ab827a4163310536ecbed45aef36476d7eed485
Witek <vytukas@o2.pl>
2022-01-20 11:22:39 +0100

    test

---
+++ Aplikacja_GOT_PTTK/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplikacja_GOT_PTTK.Models
+{
+    public class Class
+    {
+    }
+}
