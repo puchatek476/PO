e65b5e20b5dfd801fc79518d301bcd26ed7e4989
Jakub Chmielowiec <chmielowiec.ja@gmail.com>
2022-01-21 15:23:21 +0100

    add map razor view to home controller

--- Aplikacja_GOT_PTTK/Controllers/HomeController.cs
+++ Aplikacja_GOT_PTTK/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Aplikacja_GOT_PTTK.Controllers
             return View();
         }
 
+        public IActionResult Map()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
