c9201e11ee66fa9d443ffbbef6439ad4d37d4ccc
Jakub Chmielowiec <chmielowiec.ja@gmail.com>
2022-01-21 15:31:21 +0100

    move map view to separate controller

--- Aplikacja_GOT_PTTK/Controllers/HomeController.cs
+++ Aplikacja_GOT_PTTK/Controllers/HomeController.cs
@@ -28,11 +28,6 @@ namespace Aplikacja_GOT_PTTK.Controllers
             return View();
         }
 
-        public IActionResult Map()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Aplikacja_GOT_PTTK/Controllers/MapController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplikacja_GOT_PTTK.Controllers
+{
+    public class MapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
